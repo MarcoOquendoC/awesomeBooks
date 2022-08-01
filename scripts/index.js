0855a38a0f89e22dcea1a9d238ea4a0962c90e52
Lucas Comamala <lucascomamala@gmail.com>
2022-08-01 12:57:55 -0600

    Javascript Changes
    - Added array of objects to keep the book collection in
    - Added localStorage functionality to save and load our book collection

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+let bookCollection = [];
 const tree = document.createDocumentFragment();
 const addBtn = document.getElementById('addBtn');
 const bookList = document.getElementById('bookList');
@@ -8,7 +9,7 @@ function addBook() {
 
   const div = document.createElement('div');
   div.classList.add('book');
-  
+
   const bookTitle = document.createElement('p');
   bookTitle.classList.add('bookInfo');
   bookTitle.textContent = inputTitle;
@@ -18,17 +19,74 @@ function addBook() {
   author.classList.add('bookInfo');
   author.textContent = inputAuthor;
   div.appendChild(author);
-  
+
   const removeBtn = document.createElement('button');
   removeBtn.classList.add('remove');
   removeBtn.textContent = 'Remove';
   div.appendChild(removeBtn);
-  
+
   const hr = document.createElement('hr');
   div.appendChild(hr);
-  
+
+  tree.appendChild(div);
+  bookList.appendChild(tree);
+
+  const bookObj = {
+    title: inputTitle,
+    author: inputAuthor,
+  };
+  bookCollection.push(bookObj);
+
+  localStorage.setItem('data', JSON.stringify(bookCollection));
+}
+
+function addBookFromStorage(titleP, authorP) {
+  const div = document.createElement('div');
+  div.classList.add('book');
+
+  const bookTitle = document.createElement('p');
+  bookTitle.classList.add('bookInfo');
+  bookTitle.textContent = titleP;
+  div.appendChild(bookTitle);
+
+  const author = document.createElement('p');
+  author.classList.add('bookInfo');
+  author.textContent = authorP;
+  div.appendChild(author);
+
+  const removeBtn = document.createElement('button');
+  removeBtn.classList.add('remove');
+  removeBtn.textContent = 'Remove';
+  div.appendChild(removeBtn);
+
+  const hr = document.createElement('hr');
+  div.appendChild(hr);
+
   tree.appendChild(div);
   bookList.appendChild(tree);
 }
 
+// function removeBook() {
+//   const rmvBtns = document.getElementsByClassName('remove');
+//
+// }
+
 addBtn.addEventListener('click', addBook);
+
+// document.addEventListener('click', (e) => {
+//   if (e.target && e.target.classList.contains('remove')) {
+//
+//   }
+// });
+
+window.addEventListener('load', () => {
+  if (localStorage.getItem('data')) {
+    bookCollection = JSON.parse(localStorage.getItem('data'));
+  }
+
+  if (bookCollection) {
+    for (let i = 0; i < bookCollection.length; i += 1) {
+      addBookFromStorage(bookCollection[i].title, bookCollection[i].author);
+    }
+  }
+});
