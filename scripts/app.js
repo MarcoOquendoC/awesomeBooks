bd6b797693d6823faae31a0a7239791991cc8726
Lucas Comamala <lucascomamala@gmail.com>
2022-08-03 15:55:04 -0600

    Add NavBar functionality
    - Sections toggle between show and hide with nav buttons
    - Nav button styles change

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,42 @@ import {UI} from './ui.js';
 import {Book} from './book.js';
 import {Storage} from './storage.js'
 
+const s1 = document.getElementById('list');
+const s2 = document.getElementById('new');
+const s3 = document.getElementById('contact');
+const b1 = document.getElementById('navList');
+const b2 = document.getElementById('navNew');
+const b3 = document.getElementById('navContact');
+
+
+// Nav Links Events
+b1.addEventListener('click', () => {
+  s1.classList.remove('hide');
+  s2.classList.add('hide');
+  s3.classList.add('hide');
+  b1.classList.add('active');
+  b2.classList.remove('active');
+  b3.classList.remove('active');
+});
+
+b2.addEventListener('click', () => {
+  s1.classList.add('hide');
+  s2.classList.remove('hide');
+  s3.classList.add('hide');
+  b1.classList.remove('active');
+  b2.classList.add('active');
+  b3.classList.remove('active');
+});
+
+b3.addEventListener('click', () => {
+  s1.classList.add('hide');
+  s2.classList.add('hide');
+  s3.classList.remove('hide');
+  b1.classList.remove('active');
+  b2.classList.remove('active');
+  b3.classList.add('active');
+});
+
 // Event: display books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
